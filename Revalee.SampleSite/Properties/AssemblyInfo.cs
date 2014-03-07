f48b44fabd8a8f4d423a46da5df68da1cabec023
Brian Mullin <brian.mullin@sageanalytic.com>
2014-03-07 10:06:44 -0500

    Updated assembly version numbers to 2.1.2

--- Revalee.SampleSite/Properties/AssemblyInfo.cs
+++ Revalee.SampleSite/Properties/AssemblyInfo.cs
@@ -34,6 +34,6 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 [assembly: AssemblyVersion("2.0.0")]
-[assembly: AssemblyFileVersion("2.1.1")]
-[assembly: AssemblyInformationalVersion("2.1.1-alpha")]
+[assembly: AssemblyFileVersion("2.1.2")]
+[assembly: AssemblyInformationalVersion("2.1.2")]
 
